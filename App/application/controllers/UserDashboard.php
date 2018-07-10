7edffae37070627bdad44dbe9b40f17c8f96e68b
noeramadhan <noeramadhan0@gmail.com>
2018-07-11 01:08:09 +0700

    user dashboard

--- App/application/controllers/UserDashboard.php
+++ App/application/controllers/UserDashboard.php
@@ -3,9 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class UserDashboard extends CI_Controller {
 
-	public function index(){
-		$this->load->view('user-dashboard/starter');
-	}
 	public function transaksi(){
 		$this->load->view('user-dashboard/transaksi');
 	}
